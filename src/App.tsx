9d4e6f2461d3f181b2c3b550c8a3f931c0bd9b7a
SocolAndreea <andreeas.2002@yahoo.com>
2024-03-10 21:10:32 +0200

    first_commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Content from "./contents/Content";
 
 function App() {
+  const [value] = useState("hello");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {value}
+        <div>{value}</div>
+        <span>{value}</span>
+      </div>
+      <Content valueFromParent={value} />
+    </>
   );
 }
 
---
+++ src/contents/StyledComponents.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StylesFor3rdDiv = styled.div`
+  background-color: pink;
+  font-size: 24px;
+  color: yellow;
+  margin-top: 15px;
+`;
